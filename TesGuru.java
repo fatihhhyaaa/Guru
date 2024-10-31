0160c98c6f77238be208feaf8c681911f7c34332
fatihhhyaaa <friskiramdhani@gmail.com>
2024-10-31 11:56:56 +0700

    Add files via upload

---
+++ TesGuru.java
@@ -0,0 +1,51 @@
+public class TesGuru {
+    public static void main(String[]args) {
+Guru pakDiaur = new Guru();
+Guru buPhasa = new Guru();
+Guru pakBagus = new Guru();
+Guru pakAyat = new Guru();
+Guru buAulia = new Guru();
+pakDiaur.id = 1;
+pakDiaur.nama = "pak Diaur";
+pakDiaur.mapel = "IPAS";
+pakDiaur.alamat = "MALANG";
+buPhasa.id = 2;
+buPhasa.nama = "bu Phasa";
+buPhasa.mapel = "PRODUKTIF";
+buPhasa.alamat = "MALANG";
+pakBagus.id = 3;
+pakBagus.nama = "pak Bagus";
+pakBagus.mapel = "INFORMATIKA";
+pakBagus.alamat = "MALANG";
+pakAyat.id = 4;
+pakAyat.nama = "pak Ayat";
+pakAyat.mapel = "UI UX";
+pakAyat.alamat = "MALANG";
+buAulia.id = 5;
+buAulia.nama = "bu Aulia";
+buAulia.mapel = "PRODUKTIF";
+buAulia.alamat = "MALANG";
+
+
+System.out.println(pakDiaur.id);
+System.out.println(pakDiaur.nama);
+System.out.println(pakDiaur.mapel);
+System.out.println(pakDiaur.alamat);
+System.out.println(buPhasa.id);
+System.out.println(buPhasa.nama);
+System.out.println(buPhasa.mapel);
+System.out.println(buPhasa.alamat);
+System.out.println(pakBagus.id);
+System.out.println(pakBagus.nama);
+System.out.println(pakBagus.mapel);
+System.out.println(pakBagus.alamat);
+System.out.println(pakAyat.id);
+System.out.println(pakAyat.nama);
+System.out.println(pakAyat.mapel);
+System.out.println(pakAyat.alamat);
+System.out.println(buAulia.id);
+System.out.println(buAulia.nama);
+System.out.println(buAulia.mapel);
+System.out.println(buAulia.alamat);
+}
+}
